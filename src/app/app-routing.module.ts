f5ace8f6c69c30721a7aaf8f81e8ed64bdd04b11
nleclaire <noah.leclaire@gmail.com>
2021-05-11 12:21:32 -0400

    Generating topics component, adding routing for topics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TopicsComponent} from './components/topics/topics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'topics',
+    component: TopicsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
