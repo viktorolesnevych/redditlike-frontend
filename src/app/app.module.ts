2bb5bd5919145e4fb0b048b220f19bc55a735360
nleclaire <noah.leclaire@gmail.com>
2021-05-11 12:40:35 -0400

    Rudimentary API call for topics working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TopicsComponent } from './components/topics/topics.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TopicsComponent } from './components/topics/topics.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-topics',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topics.component.css']
 })
 export class TopicsComponent implements OnInit {
+  topics: any[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('http://redditlikeapi-env.eba-tpi4ysj3.us-east-2.elasticbeanstalk.com/api/topics')
+      .subscribe(response => {
+        console.log(response);
+        // this.topics = response;
+      });
   }
 
 }
